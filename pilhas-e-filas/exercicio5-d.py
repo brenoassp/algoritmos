a350c6afd46ff8958ccab5039b0f59b5ee56c776
Breno Almeida <breno_17@live.com>
2018-03-26 11:14:33 -0300

    finish ex 5-d

--- pilhas-e-filas/exercicio5-d.py
+++ pilhas-e-filas/exercicio5-d.py
@@ -9,10 +9,45 @@ class ListaDuplamenteEncadeadaCircular:
     ultimo = None
 
     def contido(self, valor):
-        return
+        if not self.ultimo:
+            return False
+        no_atual = self.primeiro
+        while no_atual != self.ultimo:
+            if no_atual.valor == valor:
+                return True
+            no_atual = no_atual.proximo
+        if no_atual and no_atual.valor == valor:
+            return True
+        return False
 
     def inserir(self, valor):
-        return
+        no = No()
+        no.valor = valor
+        if not self.ultimo:
+            self.primeiro = no
+            self.ultimo = no
+            return
+        no.proximo = self.ultimo.proximo
+        no.anterior = self.ultimo
+        self.ultimo.proximo = no
+        self.primeiro.anterior = no
+        self.ultimo = no
 
-    def remover(self):
-        return
+    def remover(self, valor):
+        if not self.contido(valor):
+            raise ValueError("o valor fornecido nao esta contido na lista")
+        if self.primeiro == self.ultimo:
+            self.primeiro = None
+            self.ultimo = None
+        else:
+            no_atual = self.primeiro
+            while no_atual:
+                if no_atual.valor == valor:
+                    break
+                no_atual = no_atual.proximo
+            if self.primeiro == no_atual:
+                self.primeiro = no_atual.proximo
+            elif self.ultimo == no_atual:
+                self.ultimo = no_atual.anterior
+            no_atual.anterior.proximo = no_atual.proximo
+            no_atual.proximo.anterior = no_atual.anterior
