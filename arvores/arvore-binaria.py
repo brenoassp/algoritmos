6d8669daee2556e731ea68b484ffca3e39623b30
Breno Almeida <breno_17@live.com>
2018-05-01 07:41:23 -0300

    finish dsw algorithm

--- arvores/arvore-binaria.py
+++ arvores/arvore-binaria.py
@@ -1,3 +1,6 @@
+import math
+
+
 class No:
 
     def __init__(self, chave):
@@ -10,6 +13,7 @@ class ArvoreBinaria:
 
     def __init__(self):
         self.raiz = None
+        self.n = 0
 
     def insere(self, no): #insere igual da abb feito apenas para testar outras funções
         if not isinstance(no, No):
@@ -25,12 +29,14 @@ class ArvoreBinaria:
                 elif no.chave < atual.chave:
                     if atual.esq is None:
                         atual.esq = no
+                        self.n += 1
                         return
                     else:
                         atual = atual.esq
                 elif no.chave > atual.chave:
                     if atual.dir is None:
                         atual.dir = no
+                        self.n += 1
                         return
                     else:
                         atual = atual.dir
@@ -69,23 +75,66 @@ def cria_espinha_dorsal(arv):
         else:
             avo = pai
             pai = pai.dir
-
     arv.raiz = raiz_nova_arvore
+
+
+def rotacao_esq(pai, avo, filho_dir):
+    if not filho_dir:
+        raise Exception("filho_dir nao pode ser None")
+    pai.dir = filho_dir.esq
+    filho_dir.esq = pai
+    if avo:
+        avo.dir = filho_dir
+    return filho_dir
+
+
+def rotacoes_esq_cada_2_nos(arv, numero_rotacoes, comeca_raiz=True):
+    if comeca_raiz:
+        avo = None
+        pai = arv.raiz
+        filho_dir = pai.dir
+    else: # começa do filho direito
+        avo = arv.raiz
+        pai = avo.dir
+        filho_dir = pai.dir
+    for i in range(numero_rotacoes):
+        if not avo:
+            arv.raiz = rotacao_esq(pai, avo, filho_dir)
+            avo = arv.raiz
+            pai = avo.dir
+            filho_dir = pai.dir
+        else:
+            avo.dir = rotacao_esq(pai, avo, filho_dir)
+            avo = avo.dir
+            pai = avo.dir
+            filho_dir = pai.dir
+
+
+def criar_arvore_altura_minima(arv):
+    m = int(math.pow(2, math.floor(math.log(arv.n + 1, 2)))) - 1
+    rotacoes_esq_cada_2_nos(arv, arv.n - m)
+    while m > 1:
+        m = int(math.floor(m / 2))
+        rotacoes_esq_cada_2_nos(arv, m, comeca_raiz=False)
 
 
 if __name__ == '__main__':
     ab = ArvoreBinaria()
 
-    ab.insere(No(20))
     ab.insere(No(15))
     ab.insere(No(17))
+    ab.insere(No(19))
+    ab.insere(No(20))
     ab.insere(No(25))
-    ab.insere(No(23))
     ab.insere(No(30))
+    ab.insere(No(32))
+    ab.insere(No(40))
+    ab.insere(No(50))
 
     # ab.imprime()
 
     cria_espinha_dorsal(ab)
+    criar_arvore_altura_minima(ab)
     print('\n')
 
     ab.imprime()
